ce8d5faf8a5ca613df99c05b97bfcc503fd6235e
Breno Rage <brenorage16@gmail.com>
2016-10-21 00:35:15 -0200

    init arc and make the transaction model

---
+++ app/src/main/java/com/example/brenorage/paymentwithcreditcardsample/model/PaymentTransaction.java
@@ -0,0 +1,70 @@
+package com.example.brenorage.paymentwithcreditcardsample.model;
+
+public class PaymentTransaction {
+
+    private String ownerName;
+    private String cardNumber;
+    private String yearValid;
+    private String monthValid;
+    private String brand;
+    private String cvv;
+    private String amountTransaction;
+
+    public PaymentTransaction() {}
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getYearValid() {
+        return yearValid;
+    }
+
+    public void setYearValid(String yearValid) {
+        this.yearValid = yearValid;
+    }
+
+    public String getMonthValid() {
+        return monthValid;
+    }
+
+    public void setMonthValid(String monthValid) {
+        this.monthValid = monthValid;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getAmountTransaction() {
+        return amountTransaction;
+    }
+
+    public void setAmountTransaction(String amountTransaction) {
+        this.amountTransaction = amountTransaction;
+    }
+}
