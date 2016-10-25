5749db42ff0189c86c0dc1838ee0b9c4ddd7ed48
Breno Rage <brenorage16@gmail.com>
2016-10-25 00:08:42 -0200

    Add activity result params in activity

--- app/src/main/java/com/example/brenorage/paymentwithcreditcardsample/view/ResultActivity.java
+++ app/src/main/java/com/example/brenorage/paymentwithcreditcardsample/view/ResultActivity.java
@@ -1,9 +1,15 @@
 package com.example.brenorage.paymentwithcreditcardsample.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.brenorage.paymentwithcreditcardsample.R;
+import com.example.brenorage.paymentwithcreditcardsample.Util.Constants;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -11,5 +17,25 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.ok_button)
+    public void okAction() {
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (resultCode) {
+            case Constants.RESULT_OK:
+                break;
+            case Constants.RESULT_ERROR_404:
+                break;
+            case Constants.RESULT_ERROR_500:
+                break;
+        }
     }
 }
