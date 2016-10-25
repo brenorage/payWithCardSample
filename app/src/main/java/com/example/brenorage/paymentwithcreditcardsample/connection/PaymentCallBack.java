7827cced8141fb16462c942ef8884fffc99dd967
Breno Rage <brenorage16@gmail.com>
2016-10-24 23:45:40 -0200

    doing some adjusts in payment presenter

---
+++ app/src/main/java/com/example/brenorage/paymentwithcreditcardsample/connection/PaymentCallBack.java
@@ -0,0 +1,8 @@
+package com.example.brenorage.paymentwithcreditcardsample.connection;
+
+/**
+ * Created by brenorage on 24/10/16.
+ */
+
+public class PaymentCallBack {
+}
---
+++ app/src/main/java/com/example/brenorage/paymentwithcreditcardsample/model/PaymentResult.java
@@ -0,0 +1,8 @@
+package com.example.brenorage.paymentwithcreditcardsample.model;
+
+/**
+ * Created by brenorage on 24/10/16.
+ */
+
+public class PaymentResult {
+}
