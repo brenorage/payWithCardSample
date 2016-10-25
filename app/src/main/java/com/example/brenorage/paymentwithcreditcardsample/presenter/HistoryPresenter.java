0216613be75a89655d6aa027c717ed673439892b
Breno Rage <brenorage16@gmail.com>
2016-10-25 00:08:17 -0200

    Init history presenter

--- app/src/main/java/com/example/brenorage/paymentwithcreditcardsample/presenter/HistoryPresenter.java
+++ app/src/main/java/com/example/brenorage/paymentwithcreditcardsample/presenter/HistoryPresenter.java
@@ -1,4 +1,14 @@
 package com.example.brenorage.paymentwithcreditcardsample.presenter;
 
+import com.example.brenorage.paymentwithcreditcardsample.model.PaymentTransaction;
+
+import java.util.List;
+
 public class HistoryPresenter {
+
+    public HistoryPresenter() {}
+
+    private List<PaymentTransaction> getTransactionList() {
+        return PaymentTransaction.listAll(PaymentTransaction.class);
+    }
 }
